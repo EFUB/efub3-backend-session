904dfaca8a8d0c2a0f5575b175aaa64f20d72d2f
chhaewxn <chaewon1019@ewhain.net>
2023-05-13 03:17:30 +0900

    ✨ feat: Comment(댓글) CR 코드

---
+++ blog/src/main/java/efub/session/blog/account/controller/AccountPostController.java
@@ -0,0 +1,32 @@
+package efub.session.blog.account.controller;
+
+import efub.session.blog.account.service.AccountService;
+import efub.session.blog.post.domain.Post;
+import efub.session.blog.post.dto.PostListResponseDto;
+import efub.session.blog.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// url: /accounts/{accountId}/posts
+
+@RestController
+@RequestMapping("/accounts/{accountId}/posts")
+@RequiredArgsConstructor
+public class AccountPostController {
+
+    // 의존관계 : AccountCommentController -> PostService
+    private final PostService postService;
+    // 의존관계 : AccountCommentController -> AccountService
+    private final AccountService accountService;
+
+    // 특정 유저의 게시글 목록 조회
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public PostListResponseDto readAccountPosts(@PathVariable Long accountId) {
+        List<Post> postList = postService.findPostListByWriter(accountId);
+        return PostListResponseDto.of(postList);
+    }
+}
