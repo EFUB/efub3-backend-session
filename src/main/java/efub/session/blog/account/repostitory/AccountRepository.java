6e509377d1a9e4fc2ffd4e48985b0eab56c8de3a
choiyounji <yunji71@naver.com>
2023-05-16 01:47:09 +0900

    feat: account 관련 기능 수정

--- src/main/java/efub/session/blog/account/repostitory/AccountRepository.java
+++ src/main/java/efub/session/blog/account/repostitory/AccountRepository.java
@@ -3,6 +3,9 @@ package efub.session.blog.account.repostitory;
 import efub.session.blog.account.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Boolean existsByEmail(String email);
+    Optional<Account> findAccountById(Long accountId);
 }
